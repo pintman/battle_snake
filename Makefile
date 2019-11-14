9c04c8dca0d6e6deb1880e7e6e4d75750a73f126
Pintman <pintman@0xabc.de>
2019-11-14 17:35:29 +0100

    added bottle based sample snakes

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ URL=https://github.com/battlesnakeio/engine/releases/download/v$(VERSION)/engine
 start_engine: release
 	cd release; ./engine server
 
+start_snakes: venv
+	venv/bin/python snake.py localhost 8081 &
+	venv/bin/python snake.py localhost 8082 &
+
 create_game: release
 	# Create game and return the game id
 	./release/engine create --config snake-config.json | cut -d ':' -f 2 | cut -d '"' -f 2
---
+++ snake.py
@@ -0,0 +1,45 @@
+import bottle
+import json
+
+@bottle.post("/start")
+def start():
+    print("start")
+    js = bottle.request.json
+    print(js)
+    return {
+        "color": "#ff00ff",
+        "headType": "bendr",
+        "tailType": "pixel"
+    }
+
+@bottle.post("/move")
+def move():
+    print("move")
+    js = bottle.request.json
+    print(js)
+
+@bottle.post("/end")
+def end():
+    print("end")
+    js = bottle.request.json
+    print(js)
+
+@bottle.post("/ping")
+def ping():
+    print("ping")
+    js = bottle.request.json
+    print(js)
+
+
+def main(host, port):
+    bottle.run(host=host, port=port)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print("python snake.py HOST PORT")
+        sys.exit()
+
+    host, port = sys.argv[1:]
+    main(host, int(port))
+    
